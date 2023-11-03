64f18605a5c711dc3425818425b4a124d171659a
ViperBite03 <layitamartin@gmail.com>
2023-11-03 01:36:01 +0100

    todo

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,49 @@
+<script lang="ts"></script>
+
+<div class="nav-bar">
+  <button id="boutme">Boutme</button>
+  <button id="boutme">Boutme</button>
+  <button id="boutme">Boutme</button>
+  <button id="boutme">Boutme</button>
+  <button id="boutme">Boutme</button>
+</div>
+
+<style lang="scss">
+  .nav-bar {
+    background-color: rgb(255, 255, 255);
+    height: fit-content;
+    width: fit-content;
+
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+
+    border-radius: 100px;
+    padding: 14px 35px;
+    left: 0;
+    right: 0;
+    top: 15px;
+    margin: auto;
+
+    button {
+      cursor: pointer;
+      padding: 7px 14px;
+      border: none;
+      border-radius: 15px;
+
+      font-size: 15px;
+      background-color: white;
+      color: black;
+      box-shadow: 0px 1px 2px rgba(166, 175, 195, 0.25);
+      transition: 0.2s ease;
+
+      &:hover {
+        background-color: #1e293b;
+        color: #fff;
+        transition: 0.2s ease;
+      }
+    }
+  }
+</style>
---
+++ src/components/Start.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  const title = "I'm Laia";
+  import NavBar from "./NavBar.svelte";
+</script>
+
+<div class="start">
+  <NavBar />
+  <div class="glass">
+    <div class="title">{title}</div>
+  </div>
+</div>
+
+<style lang="scss">
+  .start {
+    height: 100vh;
+    width: 100%;
+    position: relative;
+    display: flex;
+    align-items: center;
+
+    .glass {
+      width: 100%;
+      height: 50%;
+
+      background: rgba(255, 255, 255, 0.13);
+      box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+      backdrop-filter: blur(2px);
+      border: 1px solid rgba(255, 255, 255, 0.2);
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .title {
+        font-family: "against", sans-serif;
+
+        background-clip: text;
+        -webkit-background-clip: text;
+        background-image: url(https://i.pinimg.com/originals/d8/7e/e3/d87ee381e69fc49b0686855f65df920c.jpg);
+        background-size: cover;
+        background-position: center;
+
+        color: transparent;
+        font-size: 125px;
+      }
+    }
+  }
+</style>
